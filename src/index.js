597e86380f84c09794911757f667f20209eb4f4c
kjeks <danskejon@hotmail.com>
2018-02-22 14:45:13 +0100

    fix sass

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './public/stylesheets/MainPage.css';
+import './sass/stylesheets/MainPage.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
