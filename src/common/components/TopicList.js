aa97d8ffea781f9cb3190ce393401fe6ac58bf35
kjeks <danskejon@hotmail.com>
2018-06-04 09:05:28 +0200

    Move Topiclist logic into own component

---
+++ src/common/components/TopicList.js
@@ -0,0 +1,22 @@
+import React, {PureComponent} from 'react';
+import Topic from "./Topic";
+import CollapsibleComponent from "./CollapsibleComponent";
+
+export default class TopicList extends PureComponent {
+    render () {
+        const topicList = this.props.topicList.map(topic=> {
+            return <Topic
+                topic={topic}
+                onTopicSelected={this.props.onTopicSelected}
+                selected={topic.get('id') === this.props.selectedTopicId}
+                key={topic.get('id')}
+            />
+        });
+
+        return (
+            <CollapsibleComponent title={this.props.name} className="topic-list">
+                {topicList}
+            </CollapsibleComponent>
+        )
+    }
+}
--- src/common/components/TopicsList.js
+++ src/common/components/TopicsList.js
@@ -1,8 +1,7 @@
 import React, {PureComponent} from 'react';
-import Topic from "./Topic";
 import Immutable from 'immutable';
 import TopicListHoc from "../HOCS/TopicListHoc";
-import CollapsibleComponent from "./CollapsibleComponent";
+import TopicList from "./TopicList";
 
 class TopicsList extends PureComponent {
     static defaultProps = {
@@ -10,36 +9,10 @@ class TopicsList extends PureComponent {
         otherTopics: Immutable.List()
     };
     render() {
-
-        const favoriteTopics = this.props.favoriteTopics.map(topic => {
-            return <Topic
-                topic={topic}
-                onTopicSelected={this.props.onTopicSelected}
-                selected={topic.get('id') === this.props.selectedTopicId}
-                key={topic.get('id')}
-            />
-        });
-
-        const otherTopics = this.props.otherTopics.map(topic => {
-            return <Topic
-                topic={topic}
-                onTopicSelected={this.props.onTopicSelected}
-                selected={topic.get('id') === this.props.selectedTopicId}
-                key={topic.get('id')}
-            />
-        });
         return (
             <div className="topic-lists">
-                <CollapsibleComponent title='Favorite Topics' className="topic-list">
-                    <div className="topic-list__items">
-                        {favoriteTopics}
-                    </div>
-                </CollapsibleComponent>
-                <CollapsibleComponent title="Other Topics" className="topic-list">
-                    <div className="topic-list__items">
-                        {otherTopics}
-                    </div>
-                </CollapsibleComponent>
+                <TopicList topicList={this.props.favoriteTopics} name="favorite Topics"/>
+                <TopicList topicList={this.props.otherTopics} name="other Topics"/>
             </div>
         )
     }
